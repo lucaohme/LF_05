637608ad39b03056af5089a92feb363abbf5c896
Ohme <Ohme@172.29.55.189>
2023-01-11 12:06:03 +0100

    Aufgabe A4.4, A5.2, A5.3

--- LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
+++ LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -6,29 +6,72 @@ public class Fahrkartenautomat {
 
 		Scanner tastatur = new Scanner(System.in);
 
-		double zuZahlenderBetrag;
-		int ticketanzahl;
+		int auswahl = 0;
+		double zuZahlenderBetrag = 0.0;
+		int ticketanzahl = 0;
 		double eingezahlterGesamtbetrag;
 		double eingeworfeneMuenze;
 		double rueckgabebetrag;
 		double nochZuZahlen;
+		
+		
+		//Fahrkartenbestellvorgang
+		System.out.println("Fahrkartenbestellvorgang:");
+		System.out.println("=======================");
+		System.out.println("\nWaehlen Sie ihre Wunschfahrkarte fuer Berlin AB aus:");
+		System.out.println("\tKurzstrecke AB \t\t[2,00 EUR]\t(1)");
+		System.out.println("\tEinzelfahrschein AB \t[3,00 EUR]\t(2)");
+		System.out.println("\tTageskarte AB \t\t[8,80 EUR]\t(3)");
+		System.out.println("\t4-Fahrten-Karte AB \t[9,40 EUR]\t(4)");
 
-		// Fahrscheinausgabe
-		System.out.print("Zu zahlender Betrag (Euro): ");
+		while (auswahl < 1 || auswahl > 5) {
+			System.out.print("\nIhre Wahl: ");
+			auswahl = tastatur.nextInt();
+				if (auswahl < 1 || auswahl > 5) {
+					System.out.println(">>falsche Eingabe<<");
+				}
+		}
+
+		if (auswahl == 1) {
+			zuZahlenderBetrag = 2;
+		} else if (auswahl == 2) {
+			zuZahlenderBetrag = 3;
+		} else if (auswahl == 3) {
+			zuZahlenderBetrag = 8.80;
+		} else if (auswahl == 4) {
+			zuZahlenderBetrag = 9.40;
+		}
+		
+		/*
+		// Geldbetrag eingeben
+		System.out.print("Ticketpreis (Euro): ");
 		zuZahlenderBetrag = tastatur.nextDouble();
-		System.out.print("Anzahl der Tickets: ");
-		ticketanzahl = tastatur.nextInt();
+		if (zuZahlenderBetrag <= 0) {
+			zuZahlenderBetrag = 1;
+			System.out.println("Fehlerhafte Eingabe - Ticketpreis wurde auf 1,00 Euro gesetzt");
+		}
+		*/
+		
+		// Anzahl Tickets
+		while (ticketanzahl < 1 || ticketanzahl > 10) {
+			System.out.print("Anzahl der Tickets: ");
+			ticketanzahl = tastatur.nextInt();
+			
+			if (ticketanzahl < 1 || ticketanzahl > 10) {
+			System.out.println("Wählen Sie bitte eine eine Anzahl von 1 bis 10 Tickets aus");
+			}
+		}
+		
+		
 		
 		zuZahlenderBetrag = ticketanzahl * zuZahlenderBetrag;
 
-		// Geldbetrag eingeben
+		// Geldeinwurf
 		eingezahlterGesamtbetrag = 0.0;
 		nochZuZahlen = 0.0;
 		while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
 			nochZuZahlen = zuZahlenderBetrag - eingezahlterGesamtbetrag;
-			System.out.print("Noch zu zahlen: ");
-			System.out.printf("%.2f", nochZuZahlen);
-			System.out.printf(" Euro");
+			System.out.printf("Noch zu zahlen: %.2f Euro\n", nochZuZahlen);
 			System.out.print("\nEingabe (mind. 5 Cent, höchstens 2 Euro): ");
 			eingeworfeneMuenze = tastatur.nextDouble();
 			eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
@@ -50,9 +93,7 @@ public class Fahrkartenautomat {
 		
 		
 		if (rueckgabebetrag > 0.0) {
-			System.out.print("Der Rückgabebetrag in Höhe von ");
-			System.out.printf("%.2f", rueckgabebetrag);
-			System.out.print(" Euro");
+			System.out.printf("Der Rückgabebetrag in Höhe von %.2f Euro", rueckgabebetrag);
 			System.out.println(" wird in folgenden Münzen ausgezahlt:");
 
 			while (rueckgabebetrag >= 2.0) { // 2-Euro-Münzen
@@ -87,3 +128,5 @@ public class Fahrkartenautomat {
 		tastatur.close();
 	}
 }
+
+
