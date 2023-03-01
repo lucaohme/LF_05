25f596df60181a12a332f2f595f85554e2310a4f
Ohme <Ohme@172.29.55.189>
2023-03-01 12:27:45 +0100

    A7.3: Das Fahrkartenarray

--- LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
+++ LS2_Fahrkartenautomat/src/Fahrkartenautomat.java
@@ -13,20 +13,9 @@ public class Fahrkartenautomat {
 		begruessung();
 		zuZahlenderBetrag = fahrkartenbestellungErfassen(tastatur);
 		eingezahlterGesamtbetrag = fahrkartenBezahlen(tastatur, zuZahlenderBetrag);
-		// Geldeinwurf
-		
+		fahrkartenAusgeben();
 
 		// Rückgeldberechnung und -ausgabe
-		System.out.println("\nFahrschein wird ausgegeben");
-		for (int i = 0; i < 8; i++) {
-			System.out.print("=");
-			try {
-				Thread.sleep(200);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		System.out.println("\n\n");
 
 		rueckgabebetrag = eingezahlterGesamtbetrag - zuZahlenderBetrag;
 
@@ -67,69 +56,52 @@ public class Fahrkartenautomat {
 
 	}
 
-	//Begruessung
+	// Begruessung
 	static void begruessung() {
 		System.out.println("Herzlich Willkommen!");
+		System.out.println("\nWaehlen Sie eine Fahrkarte aus:");
 	}
 
 	// Kartenauswahl und Tickets
 	static double fahrkartenbestellungErfassen(Scanner tastatur) {
-		
+
 		int auswahl = 0;
 		double zuZahlenderBetrag = 0.0;
-	
+
 		double gesamt = 0;
 
 		while (true) {
 
-			System.out.println("\nWaehlen Sie ihre Wunschfahrkarte fuer Berlin AB aus:");
-			System.out.println("\tKurzstrecke AB \t\t[2,00 EUR]\t(1)");
-			System.out.println("\tEinzelfahrschein AB \t[3,00 EUR]\t(2)");
-			System.out.println("\tTageskarte AB \t\t[8,80 EUR]\t(3)");
-			System.out.println("\t4-Fahrten-Karte AB \t[9,40 EUR]\t(4)");
-			System.out.println("\tBezahlen \t\t\t\t(9)");
+			double[] Fahrkartenpreise = { 3.0, 3.5, 3.8, 2.0, 8.6, 9.2, 10.0, 9.4, 12.6, 13.8, 25.5, 26.0, 26.5 };
+			String[] Fahrkartenbezeichnung = { "Einzelfahrschein AB", "Einzelfahrschein BC", "Einzelfahrschein ABC",
+					"Kurzstrecke AB", "Tageskarte AB", "Tageskarte BC", "Tageskarte ABC", "4-Fahrten-Karte AB",
+					"4-Fahrten-Karte BC", "4-Fahrten-Karte ABC", "Kleingruppen-Tageskarte AB",
+					"Kleingruppen-Tageskarte BC", "Kleingruppen-Tageskarte ABC" };
+
+			for (int i = 0; i < Fahrkartenpreise.length; i++) {
+				System.out.printf("%-30s %10.2f EUR %5d\n", Fahrkartenbezeichnung[i], Fahrkartenpreise[i], i + 1);
+			}
+			System.out.println("Bezahlen \t\t\t\t\t 0");
 
 			while (true) {
 				System.out.print("\nIhre Wahl: ");
 				auswahl = tastatur.nextInt();
-				
-				if (!((auswahl>0 && auswahl<5) || auswahl == 9)) {
-			   // if (( auswahl <1 || auswahl >9) || (auswahl > 4 && auswahl <9)) {
+
+				if (!((auswahl >= 0 && auswahl < Fahrkartenbezeichnung.length + 1))) {
 					System.out.println(">>falsche Eingabe<<");
 				} else {
 					break;
 				}
-
 			}
 
-			if (auswahl == 1) {
-				zuZahlenderBetrag = 2;
-			} else if (auswahl == 2) {
-				zuZahlenderBetrag = 3;
-			} else if (auswahl == 3) {
-				zuZahlenderBetrag = 8.80;
-			} else if (auswahl == 4) {
-				zuZahlenderBetrag = 9.40;
-			} else if (auswahl == 9) {
+			if (auswahl == 0)
 				break;
-			}
-			
-			/*
-			 // Geldbetrag eingeben
-			 System.out.print("Ticketpreis (Euro): ");
-			 zuZahlenderBetrag = tastatur.nextDouble();
-			 
-			
-			 if (zuZahlenderBetrag <= 0) {
-				 zuZahlenderBetrag = 1;
-				 System.out.println("Fehlerhafte Eingabe - Ticketpreis wurde auf 1,00 Euro gesetzt"); }
-			 */
-			
+
+			zuZahlenderBetrag = Fahrkartenpreise[auswahl - 1];
 
 			// Anzahl Tickets
 
 			int ticketanzahl = 0;
-			
 			while (ticketanzahl < 1 || ticketanzahl > 10) {
 				System.out.print("Anzahl der Tickets: ");
 				ticketanzahl = tastatur.nextInt();
@@ -138,7 +110,7 @@ public class Fahrkartenautomat {
 					System.out.println("Wählen Sie bitte eine eine Anzahl von 1 bis 10 Tickets aus");
 				}
 			}
-			
+
 			zuZahlenderBetrag = ticketanzahl * zuZahlenderBetrag;
 			gesamt = gesamt + zuZahlenderBetrag;
 			System.out.printf("\nZu zahlender Betrag: %.2f Euro\n", zuZahlenderBetrag);
@@ -147,22 +119,36 @@ public class Fahrkartenautomat {
 		}
 		return gesamt;
 	}
-		
-		//Geldeinwurf
-		static double fahrkartenBezahlen (Scanner tastatur, double zuZahlenderBetrag) {
-			
-			double eingezahlterGesamtbetrag = 0.0;
-			double nochZuZahlen = 0.0;
-			double eingeworfeneMuenze;
-			
-			while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
-				nochZuZahlen = zuZahlenderBetrag - eingezahlterGesamtbetrag;
-				System.out.printf("Noch zu zahlen: %.2f Euro\n", nochZuZahlen);
-				System.out.print("\nEingabe (mind. 5 Cent, höchstens 2 Euro): ");
-				eingeworfeneMuenze = tastatur.nextDouble();
-				eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
-			}
-			
-			return eingezahlterGesamtbetrag;
+
+	// Geldeinwurf
+	static double fahrkartenBezahlen(Scanner tastatur, double zuZahlenderBetrag) {
+
+		double eingezahlterGesamtbetrag = 0.0;
+		double nochZuZahlen = 0.0;
+		double eingeworfeneMuenze;
+
+		while (eingezahlterGesamtbetrag < zuZahlenderBetrag) {
+			nochZuZahlen = zuZahlenderBetrag - eingezahlterGesamtbetrag;
+			System.out.printf("Noch zu zahlen: %.2f Euro\n", nochZuZahlen);
+			System.out.print("\nEingabe (mind. 5 Cent, höchstens 2 Euro): ");
+			eingeworfeneMuenze = tastatur.nextDouble();
+			eingezahlterGesamtbetrag = eingezahlterGesamtbetrag + eingeworfeneMuenze;
+		}
+
+		return eingezahlterGesamtbetrag;
 	}
+
+	static void fahrkartenAusgeben() {
+		System.out.println("\nFahrschein wird ausgegeben");
+		for (int i = 0; i < 8; i++) {
+			System.out.print("=");
+			try {
+				Thread.sleep(200);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("\n\n");
+	}
+
 }
